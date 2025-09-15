fede613847389c5b8b6d3431351829d4e51b0951
kotov0820-del <kotov0820@gmail.com>
2025-09-15 23:12:46 +0300

    раткое описание изменений

---
+++ src/Задание4.java
@@ -0,0 +1,11 @@
+public class Задание4 {
+    public static void main(String[] args) {
+        System.out.println("Первые 10 чисел гармонического ряда:");
+
+        double sum = 0.0;
+        for (int i = 1; i <= 10; i++) {
+            sum += 1.0 / i;
+            System.out.printf("H%d = %.4f%n", i, sum);
+        }
+    }
+}
---
+++ src/Задание5.java
@@ -0,0 +1,16 @@
+public class Задание5 {
+    // Метод для вычисления факториала
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int number = 5; // можно поменять на любое число
+        long fact = factorial(number);
+        System.out.println("Факториал числа " + number + " = " + fact);
+    }
+}

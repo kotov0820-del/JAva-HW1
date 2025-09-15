93cb316ec30a8c26f63439b8e64f536c1804f8ce
kotov0820-del <kotov0820@gmail.com>
2025-09-15 22:55:17 +0300

    апример: добавлен Main.java и исправлен .gitignore

---
+++ src/Задание1.java
@@ -0,0 +1,16 @@
+public class Задание1 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
+    }
+}

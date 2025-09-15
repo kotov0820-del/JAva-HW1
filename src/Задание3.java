8df9876daea7bc70bfc83ad6e1caa64e602bb134
kotov0820-del <kotov0820@gmail.com>
2025-09-15 23:09:14 +0300

    раткое описание изменений

---
+++ src/Задание3.java
@@ -0,0 +1,15 @@
+public class Задание3{
+        public static void main(String[] args) {
+            // Проверяем, есть ли вообще аргументы
+            if (args.length == 0) {
+                System.out.println("Аргументы командной строки не переданы.");
+            } else {
+                System.out.println("Аргументы командной строки:");
+                // Выводим все аргументы в цикле for
+                for (int i = 0; i < args.length; i++) {
+                    System.out.println("Аргумент " + (i + 1) + ": " + args[i]);
+                }
+            }
+        }
+    }
+
